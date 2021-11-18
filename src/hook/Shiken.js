4b313c402cd0ecfc83a9940d55ae5d99d2967a6b
tvhung <truonghungvatvo@gmail.com>
2021-11-18 13:20:20 +0700

    push 1
    push 2
    
    push 3

--- src/hook/Shiken.js
+++ src/hook/Shiken.js
@@ -1,15 +1,15 @@
 import { useState } from "react";
 
 function Shiken(props) {
-    const [list, setList] = useState(props);
-    const [index, setIndex] = useState();
+  const [list, setList] = useState(props);
+  const [index, setIndex] = useState();
+  //push 2
+  const search = (string) => {
+    let i = list.indexOf(string);
+    setIndex(i !== -1 ? i + 1 : "NaN");
+  };
 
-    const search = (string) => {
-        let i = list.indexOf(string);
-        setIndex(i !== -1 ? i + 1 : "NaN");
-    };
-
-    return [index, search];
+  return [index, search];
 }
 
 export default Shiken;
